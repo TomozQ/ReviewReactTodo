77d433aef97ab0a4cb550089dcfee73c96e5ee24
TomozQ <63640328+TomozQ@users.noreply.github.com>
2020-11-28 21:03:19 +0900

    css途中

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,33 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <div className="inputSpace">
+        <input type="text" placeholder="Input Todo" />
+        <button>Todo</button>
+      </div>
+      <div className="incompleteSpace">
+        <ul>
+          <div className="todo">
+            <li>React入門</li>
+            <button>Complete</button>
+            <button>Cancel</button>
+          </div>
+          <div className="todo">
+            <li>React復習</li>
+            <button>Complete</button>
+            <button>Cancel</button>
+          </div>
+        </ul>
+      </div>
+      <div className="completeSpace">
+        <ul>
+          <div className="todo">
+            <li>CodeSandboxの紹介</li>
+            <button>Return</button>
+          </div>
+        </ul>
+      </div>
+    </>
   );
 }
