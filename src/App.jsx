bf4b93c90545355d9b8a4eee3ee0dd492f6a0aac
TomozQ <63640328+TomozQ@users.noreply.github.com>
2020-11-29 00:47:58 +0900

    addTodoFunction

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [todoText, setTodoText] = useState("");
+  const [inCompleteTodos, setInCompleteTodos] = useState([]);
+
+  const onChangeTodoText = (e) => setTodoText(e.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...inCompleteTodos, todoText];
+    setInCompleteTodos(newTodos);
+    setTodoText("");
+  };
+
   return (
     <>
       <div className="inputSpace">
-        <input type="text" placeholder="Input Todo" />
-        <button>Todo</button>
+        <input
+          onChange={onChangeTodoText}
+          type="text"
+          placeholder="Input Todo"
+          value={todoText}
+        />
+        <button onClick={onClickAdd}>Todo</button>
       </div>
       <div className="incompleteSpace">
         <p>INCOMPLETE</p>
         <ul>
-          <div className="todo">
-            <li>React入門</li>
-            <button>Complete</button>
-            <button>Cancel</button>
-          </div>
-          <div className="todo">
-            <li>React復習</li>
-            <button>Complete</button>
-            <button>Cancel</button>
-          </div>
+          {inCompleteTodos.map((todo, index) => {
+            return (
+              <div key={todo} className="todo">
+                <li>{todo}</li>
+                <button>Complete</button>
+                <button>Cancel</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
       <div className="completeSpace">
